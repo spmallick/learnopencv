19ee5f143cfbb975de76a76e58f222cfedc5c3b0
Satya Mallick <spmallick@gmail.com>
2015-02-25 21:27:45 -0800

    Fixing detector with params for OpenCV 3

--- BlobDetector/blob.cpp
+++ BlobDetector/blob.cpp
@@ -13,52 +13,64 @@ using namespace std;
 int main( int argc, char** argv )
 {
 
-    // Read image
-    Mat im = imread( "blob.jpg", CV_LOAD_IMAGE_GRAYSCALE );
-    
-    // Setup SimpleBlobDetector parameters.
-    SimpleBlobDetector::Params params;
-    
-    // Change thresholds
-    params.minThreshold = 10;
-    params.maxThreshold = 200;
-    
-    // Filter by Area.
-    params.filterByArea = true;
-    params.minArea = 1500;
-    
-    // Filter by Circularity
-    params.filterByCircularity = true;
-    params.minCircularity = 0.1;
-    
-    // Filter by Convexity
-    params.filterByConvexity = true;
-    params.minConvexity = 0.87;
-    
-    // Filter by Inertia
-    params.filterByInertia = true;
-    params.minInertiaRatio = 0.01;
-    
-    // Set up detector with params
-    SimpleBlobDetector detector(params);
-    
-    // Storage for blobs
-    std::vector<KeyPoint> keypoints;
-    
-    // Detect blobs
-    detector.detect( im, keypoints);
-    
-    // Draw detected blobs as red circles.
-    // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
-    // the size of the circle corresponds to the size of blob
-    
-    Mat im_with_keypoints;
-    drawKeypoints( im, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    
-    // Show blobs
-    imshow("keypoints", im_with_keypoints );
-    waitKey(0);
-    
+	// Read image
+	Mat im = imread( "blob.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+
+	// Setup SimpleBlobDetector parameters.
+	SimpleBlobDetector::Params params;
+
+	// Change thresholds
+	params.minThreshold = 10;
+	params.maxThreshold = 200;
+
+	// Filter by Area.
+	params.filterByArea = true;
+	params.minArea = 1500;
+
+	// Filter by Circularity
+	params.filterByCircularity = true;
+	params.minCircularity = 0.1;
+
+	// Filter by Convexity
+	params.filterByConvexity = true;
+	params.minConvexity = 0.87;
+
+	// Filter by Inertia
+	params.filterByInertia = true;
+	params.minInertiaRatio = 0.01;
+
+
+	// Storage for blobs
+	std::vector<KeyPoint> keypoints;
+
+
+#if CV_MAJOR_VERSION < 3   // If you are using OpenCV 2
+
+	// Set up detector with params
+	SimpleBlobDetector detector(params);
+
+	// Detect blobs
+	detector.detect( im, keypoints);
+#else 
+
+	// Set up detector with params
+	Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);   
+
+	// Detect blobs
+	detector->detect( im, keypoints);
+#endif 
+
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
+	// the size of the circle corresponds to the size of blob
+
+	Mat im_with_keypoints;
+	drawKeypoints( im, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
+	// Show blobs
+	imshow("keypoints", im_with_keypoints );
+	waitKey(0);
+
 
 }
 
