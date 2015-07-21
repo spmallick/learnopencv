45a46897bb27b91ba3b8ebc56122a94a60632fcc
Satya Mallick <spmallick@gmail.com>
2015-07-21 10:19:43 -0700

    fixed typo -- changed im1 to im2

--- ImageAlignment/image_alignment_simple_example.py
+++ ImageAlignment/image_alignment_simple_example.py
@@ -48,11 +48,11 @@ if __name__ == '__main__':
 
 
     if warp_mode == cv2.MOTION_HOMOGRAPHY :
-        # Use warpAffine for Translation, Euclidean and Affine
+        # Use warpPerspective for Homography
         im2_aligned = cv2.warpPerspective (im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
     else :
-        # Use warpPerspective for Homography
-        im2_aligned = cv2.warpAffine(im1, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+        # Use warpAffine for Translation, Euclidean and Affine
+        im2_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
 
     # Show final results
     cv2.imshow("Image 1", im1)
