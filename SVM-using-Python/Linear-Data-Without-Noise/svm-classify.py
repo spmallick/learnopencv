6cbf4d5222d6ead4b559cd64b64d5d5364bd9953
Satya Mallick <spmallick@gmail.com>
2018-07-26 11:37:40 -0700

    clean up SVM code

--- SVM-using-Python/Linear-Data-Without-Noise/svm-classify.py
+++ SVM-using-Python/Linear-Data-Without-Noise/svm-classify.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 from sklearn import svm
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import numpy as np
 import sys 
 import os 
 
-sys.path.append(os.path.abspath("../../"))
+sys.path.append(os.path.abspath("../"))
 from utils import read_data, plot_data, plot_decision_function
   
 # Read data
-x, labels = read_data("../points_rightside.txt", "../points_leftside.txt")
+x, labels = read_data("points_class_0.txt", "points_class_1.txt")
 
 # Split data to train and test on 80-20 ratio
 X_train, X_test, y_train, y_test = train_test_split(x, labels, test_size = 0.2, random_state=0)
 
+# Plot traning and test data
 plot_data(X_train, y_train, X_test, y_test)
+
   
 # Create a linear SVM classifier 
 clf = svm.SVC(C = 1.0, kernel='linear')
