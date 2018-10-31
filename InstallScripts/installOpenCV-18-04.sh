7756ed16700e105413a0fefe60fb6ece620ebd8b
vishwesh5 <vishweshshrimali5@gmail.com>
2018-10-31 20:02:04 +0530

    Update installOpenCV-18-04.sh

--- InstallScripts/installOpenCV-18-04.sh
+++ InstallScripts/installOpenCV-18-04.sh
@@ -132,6 +132,7 @@ echo "Complete"
 echo "Downloading opencv and opencv_contrib"
 git clone https://github.com/opencv/opencv.git
 cd opencv
+git fetch --all --tags --prune
 if [ "$cvVersionChoice" -eq 2 ]; then
 	git checkout $cvVersion
 else
@@ -141,6 +142,7 @@ cd ..
 
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
+git fetch --all --tags --prune
 if [ "$cvVersionChoice" -eq 2 ]; then
 	git checkout $cvVersion
 else
