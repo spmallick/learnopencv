5e0bcc71d76bf459e5ea4c0dcde3358e5f790922
Ubuntu <ubuntu@ip-172-31-6-224.us-west-2.compute.internal>
2018-11-22 10:57:03 +0000

    Add files

--- InstallScripts/Windows-3/main.py
+++ InstallScripts/Windows-3/main.py
@@ -1,19 +1,11 @@
 def main():
 	print("Welcome to OpenCV Installation by LearnOpenCV")
-	print("What OpenCV version would you like to install?")
-	cvVersionChoice=input("1 for 3.4.1 (default) and 2 for Master branch: ")
-	if cvVersionChoice != '2':
-		cvVersionChoice='1'
-	print(cvVersionChoice)
-	return cvVersionChoice
 
-def writeBatchFile(batchFile,cvVersionChoice):
+def writeBatchFile(batchFile):
 	newBatchFileName = batchFile[:-4]+"_modified.bat"
 	f_write = open(newBatchFileName,'w')
 	f_write.write("@echo off\n")
 	f_write.write("setlocal enabledelayedexpansion\n")
-	cvVersionString = "set cvVersionChoice="+cvVersionChoice+"\n"
-	f_write.write(cvVersionString)
 	
 	writeFlag=0
 	with open(batchFile,'r') as f:
@@ -28,8 +20,6 @@ def writeBatchFile(batchFile,cvVersionChoice):
 	
 	f_write.write("echo @echo off>>runScript.bat\n")
 	f_write.write("echo setlocal enabledelayedexpansion >> runScript.bat")
-	cvVersionString = "echo set cvVersionChoice="+cvVersionChoice+" >> runScript.bat\n"
-	f_write.write(cvVersionString)
 	writeFlag=0
 	with open(batchFile,'r') as f:
 		for line in f.readlines():
@@ -43,4 +33,4 @@ def writeBatchFile(batchFile,cvVersionChoice):
 
 if __name__=="__main__":
 	cvVersionChoice=main()
-	writeBatchFile("installOpenCV.bat",cvVersionChoice)
+	writeBatchFile("installOpenCV.bat")
