b7aa45a790cbefdbb69d76cf11dcbb031c7dc653
kaustubh sadekar <42736936+kaustubh-sadekar@users.noreply.github.com>
2020-02-25 14:51:16 +0530

    Changed the image file path

--- CameraCalibration/cameraCalibration.py
+++ CameraCalibration/cameraCalibration.py
@@ -21,7 +21,7 @@ objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
 # Extracting path of individual image stored in a given directory
-images = glob.glob('../images/*.jpg')
+images = glob.glob('./images/*.jpg')
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
