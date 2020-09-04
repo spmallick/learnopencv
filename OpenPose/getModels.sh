507cb961898fde208d5ff80678cf80124d1280b7
ChetanPatil28 <pchetan481@gmail.com>
2020-09-04 11:47:30 +0530

    Fixed minor issue in donwload-link

--- OpenPose/getModels.sh
+++ OpenPose/getModels.sh
@@ -2,12 +2,12 @@
 
 
 # Downloading the pose-model trained on COCO
-COCO_POSE_URL="https://www.dropbox.com/s/2h2bv29a130sgrk/pose_iter_440000.caffemodel?dl=1"
+COCO_POSE_URL="https://www.dropbox.com/s/2h2bv29a130sgrk/pose_iter_440000.caffemodel"
 COCO_FOLDER="pose/coco/"
 wget -c ${COCO_POSE_URL} -P ${COCO_FOLDER}
 
 # Downloading the pose-model trained on MPI
-MPI_POSE_URL="https://www.dropbox.com/s/drumc6dzllfed16/pose_iter_160000.caffemodel?dl=1"
+MPI_POSE_URL="https://www.dropbox.com/s/drumc6dzllfed16/pose_iter_160000.caffemodel"
 MPI_FOLDER="pose/mpi/"
 wget -c ${MPI_POSE_URL} -P ${MPI_FOLDER}
 
