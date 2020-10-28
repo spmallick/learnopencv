47f16a34a75d75dc8cf69f5ef4b0d466c5345c1c
Jatin Prakash <jatinprakash@Jatins-MacBook-Air.local>
2020-10-28 18:17:19 +0530

    Add Triple MRnet code

---
+++ MRnet-MultiTask-Approach/config.py
@@ -0,0 +1,13 @@
+config = {
+    'max_epoch' : 50,
+    'log_train' : 50,
+    'lr' : 1e-5,
+    'starting_epoch' : 0,
+    'batch_size' : 1,
+    'log_val' : 10,
+    'task' : 'combined',
+    'weight_decay' : 0.01,
+    'patience' : 5,
+    'save_model' : 1,
+    'exp_name' : 'test'
+}
---
+++ MRnet-MultiTask-Approach/models/__init__.py
@@ -0,0 +1,1 @@
+from .MRnet import MRnet
