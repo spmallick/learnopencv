7a54e71e4277f92d0f80ad2164232f272f7ae0c2
Lipi Deepaakshi Patnaik <40713338+lipi17dpatnaik@users.noreply.github.com>
2020-11-09 19:15:56 +0530

    Update SVM_Classification.py

--- SVM_Python_Cpp/SVM_Classification.py
+++ SVM_Python_Cpp/SVM_Classification.py
@@ -15,8 +15,10 @@ n_clusters_per_class=1,n_redundant=0)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
 
 ## Normalize data
-X_train = StandardScaler().fit_transform(X_train)
-X_test = StandardScaler().transform(X_test)
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 ## Training the SVC model
 # make a SVC classifier
