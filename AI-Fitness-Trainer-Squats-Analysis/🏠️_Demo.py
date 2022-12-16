e0cf4a61d84edefb51eb4a6e3d0ada5cc2ce104f
Kunaldawn7 <kunaldawn.97@gmail.com>
2022-12-16 16:45:16 +0530

    Added scripts and supported visuals for AI fitness trainer: Squats analysis blog

---
+++ AI-Fitness-Trainer-Squats-Analysis/🏠️_Demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title('AI Fitness Trainer: Squats Analysis')
+
+
+recorded_file = 'output_sample.mp4'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+    
+    
+
+    
+
+
